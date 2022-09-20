c3bcbc5143b414ee5dc58bc39a730150fa18a3bd
Nabil Noh <19523841+mrnabilnoh@users.noreply.github.com>
2022-09-20 10:28:15 +0800

    chore: parsing-log-files done

---
+++ go/parsing-log-files/parsing_log_files.go
@@ -0,0 +1,24 @@
+package parsinglogfiles
+
+import "regexp"
+
+func IsValidLine(text string) bool {
+	re := regexp.MustCompile(`\[TRC\]|\[DBG\]|\[INF\]|\[WRN\]|\[ERR\]|\[FTL\]`)
+	return re.MatchString(text)
+}
+
+func SplitLogLine(text string) []string {
+	panic("Please implement the SplitLogLine function")
+}
+
+func CountQuotedPasswords(lines []string) int {
+	panic("Please implement the CountQuotedPasswords function")
+}
+
+func RemoveEndOfLineText(text string) string {
+	panic("Please implement the RemoveEndOfLineText function")
+}
+
+func TagWithUserName(lines []string) []string {
+	panic("Please implement the TagWithUserName function")
+}
